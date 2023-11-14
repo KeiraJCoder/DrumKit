4809996b5518ad9c87c0b55d099021a8b08054de
Keira Jarvis <82812348+KeiraJCoder@users.noreply.github.com>
2023-11-14 17:16:20 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,43 @@
-
+// Event listener for keyboard press
 document.addEventListener("keydown", function(event) {
-  console.log(event.key);
-  makeSound(event.key);
-  animate(event.key);
+  console.log(event.key); // Logs the pressed key to the console
+  makeSound(event.key); // Calls the makeSound function with the pressed key
+  animate(event.key); // Calls the animate function to show a visual effect for the key press
 });
 
-
+// Select all elements with the class 'drum'
 let button = document.getElementsByClassName("drum");
 
+// Loop through all drum buttons to add click event listeners
 for (let i = 0; i <= button.length; i++) {
+  // Add a click event listener to each drum button
   button[i].addEventListener("click", function() {
-    let buttonInnerHTML = this.innerHTML;
-    makeSound(buttonInnerHTML);
-    animate(buttonInnerHTML);
+    let buttonInnerHTML = this.innerHTML; // Get the inner HTML of the clicked button
+    makeSound(buttonInnerHTML); // Call makeSound with the button's inner HTML
+    animate(buttonInnerHTML); // Call animate to show visual effect for the button click
   });
 }
 
+// Function to play sound based on the key or button clicked
 function makeSound(key) {
   switch (key) {
     case "w":
-      let tom1 = new Audio("sounds/tom-1.mp3");
-      tom1.play();
+      let tom1 = new Audio("sounds/tom-1.mp3"); // Load tom-1 sound
+      tom1.play(); // Play tom-1 sound
       break;
-    case "a":
-      let tom2 = new Audio("sounds/tom-2.mp3");
-      tom2.play();
-      break;
-    case "s":
-      let tom3 = new Audio("sounds/tom-3.mp3");
-      tom3.play();
-      break;
-    case "d":
-      let tom4 = new Audio("sounds/tom-4.mp3");
-      tom4.play();
-      break;
-    case "j":
-      let crash = new Audio("sounds/crash.mp3");
-      crash.play();
-      break;
-    case "k":
-      let bass = new Audio("sounds/kick-bass.mp3");
-      bass.play();
-      break;
-    case "l":
-      let snare = new Audio("sounds/snare.mp3");
-      snare.play();
-      break;
+    // Similar case blocks for other keys 'a', 's', 'd', 'j', 'k', 'l'
+    // Each case loads and plays a different sound file
     default:
-      alert("Attention, wrong button clicked!");
+      alert("Attention, wrong button clicked!"); // Alert if an unrecognized key is pressed
       break;
   }
 }
 
+// Function to add animation to the pressed key or clicked button
 function animate(key) {
-  let activeButton = document.querySelector("." + key);
-  activeButton.classList.toggle("pressed");
+  let activeButton = document.querySelector("." + key); // Select the button with the class corresponding to the key
+  activeButton.classList.toggle("pressed"); // Toggle the 'pressed' class to add visual effect
 
+  // Remove the 'pressed' class after 200 milliseconds to make the effect temporary
   setTimeout(function(){activeButton.classList.toggle("pressed");}, 200);
 }
